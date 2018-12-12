a6f1d90bbfff2114402f7d4d004740cdbc98c213
jhan53 <jhan53@jhu.edu>
2018-12-11 20:50:46 -0500

    integration

--- utils.py
+++ utils.py
@@ -96,3 +96,33 @@ def proj(x, _lambda):
     else:
         x_proj = x
     return x_proj
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def get_cifar10(folder):
+    tr_data = np.empty((0,32*32*3))
+    tr_labels = np.empty(1)
+    '''
+    32x32x3
+    '''
+    for i in range(1,6):
+        fname = os.path.join(folder, "%s%d" % ("data_batch_", i))
+        data_dict = unpickle(fname)
+        if i == 1:
+            tr_data = data_dict['data']
+            tr_labels = data_dict['labels']
+        else:
+            tr_data = np.vstack((tr_data, data_dict['data']))
+            tr_labels = np.hstack((tr_labels, data_dict['labels']))
+
+    data_dict = unpickle(os.path.join(folder, 'test_batch'))
+    te_data = data_dict['data']
+    te_labels = np.array(data_dict['labels'])
+
+    bm = unpickle(os.path.join(folder, 'batches.meta'))
+    label_names = bm['label_names']
+    return tr_data, tr_labels, te_data, te_labels, label_names
