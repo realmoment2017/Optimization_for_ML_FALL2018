761aa8be22f287584c5505c0184554e30148db5b
Bunny0411 <33615191+Bunny0411@users.noreply.github.com>
2018-12-13 15:17:01 -0500

    Add files via upload

--- batchGradient.py
+++ batchGradient.py
@@ -8,25 +8,26 @@ import time
 import numpy as np
 
 class batchGradient():
-    def __init__(self, X, y, iterations):
+    def __init__(self, X, y, epochs, lmbda):
         self.X = X
         self.y = y
-        self.lmbda = 150
-        self.w = []
-        self.times = []
+        self.lmbda = lmbda
+        self.w = [np.zeros((self.X.shape[1], 1))]
+        self.times = [0.0]
         self.start_time = time.time()
-        self.T = iterations
+        self.T = epochs
         
     def fit(self):
-        radius = 1.0 / (self.lmbda ** 0.5)
+        radius = 1.0 / ((10.0 * self.lmbda) ** 0.5)
         N, dim = self.X.shape[0], self.X.shape[1]
         print ("Batch gradient descent training")
         w = np.zeros((1, dim))
         W = w
+        self.y = self.y.reshape(N, 1)
         for t in range(self.T):        
-            eta = 1.0 / (self.lmbda * (t + 1)**2.0)
+            eta = 1.0 / (self.lmbda * 10.0)
             q = w - eta * self.lmbda * w
-            plane = np.array(self.y) * np.array(self.X @ np.matrix(w).transpose())
+            plane = np.multiply(self.y , np.array(self.X @ np.matrix(w).transpose()))
             mask = (plane < 1)
             labels_tmp = self.y[mask]
             k = np.sum(eta * labels_tmp.reshape(len(labels_tmp), 1) * self.X[np.resize(mask,(len(mask),dim))].reshape(len(labels_tmp), dim), axis=0) / N
@@ -38,9 +39,9 @@ class batchGradient():
             else:
                 w = q
                 
-            W += w           
+            #W += w           
             self.times.append(time.time() - self.start_time)        
-            t_w = W / (t + 1)
-            self.w.append(t_w)
+            #t_w = W / (t + 1)
+            self.w.append(w.T)
             
         return self.w, self.times
